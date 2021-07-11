efebbb3cba8fab0185615f48ccada67367cf1eae
Rafael <ra204990@students.ic.unicamp.br>
2021-07-11 17:03:51 -0300

    report rl

--- rl_algorithm/search/qLearningAgents.py
+++ rl_algorithm/search/qLearningAgents.py
@@ -18,24 +18,29 @@ class QLearningAgent(Agent):
         self.gamma = float(gamma)
         # num_training: number of training episodes
         self.num_training = int(numTraining)
-        # number of games played
+        # number of episodes played
         self.games_played = 0
+        # scores for the last 100 episodes
         self.last_100_scores = []
+        # number of games won in the last 100 episodes
         self.games_won_last_100 = 0
+        # maximum number of games won in a 100 episode interval
         self.max_games_won_overall_100 = 0
         if 'discretization' not in args:
             args['discretization'] = 'small'
         self.discrete_os_win_size = self.get_discretization(args['discretization'])
-        # Q-values
-        self.q_value = util.Counter()
+        # Q Table
+        self.q_table = util.Counter()
         # current score
         self.score = 0
         # last state
         self.lastState = []
         # last action
         self.lastAction = []
+        # direction mapping to numbers
         self.direction_map = {'East': 0, 'West': 1, 'North': 2, 'South': 3, 'Stop': 4}
 
+    # return a discretization window
     def get_discretization(self, opt):
         if opt == 'small':
             DISCRETE_OS_SIZE = [17, 4, 17, 4, 17, 4]
@@ -60,6 +65,7 @@ class QLearningAgent(Agent):
     def set_epsilon(self, value):
         self.epsilon = value
 
+    # update epsilon after each episode
     def update_epsilon(self):
         new_epsilon = (1 - float(self.get_games_played()) / self.get_num_training()) / 10.0
         self.set_epsilon(new_epsilon)
@@ -67,54 +73,64 @@ class QLearningAgent(Agent):
     def set_alpha(self, value):
         self.alpha = value
 
-    def getQValue(self, state, action):
+    # return a value from the Q table
+    def get_q_value(self, state, action):
         discrete_state = self.get_discrete_state(state)
         index = discrete_state + tuple([self.direction_map[action]])
-        return self.q_value[index]
+        return self.q_table[index]
 
+    # return a discrete state given a state
     def get_discrete_state(self, state):
+        # pacman position
         f_state = state.getPacmanPosition()
         distances = {}
         for position in state.getGhostPositions():
             distances[position] = [distance.euclidean(state.getPacmanPosition(), position)]
 
+        # position of the closest ghost
         f_state += min(distances, key=distances.get)
-        f_state += self.getClosestFood(state)
+        # position of the closest food
+        f_state += self.get_closest_food(state)
+        # discretization function
         discrete_state = f_state / self.discrete_os_win_size
         ret_value = tuple(discrete_state.astype(np.int))
         return ret_value
 
-    # return the maximum Q of state
-    def getMaxQ(self, state):
+    # return the optimal action
+    def get_max_q_value(self, state):
         q_list = []
         for action in state.getLegalPacmanActions():
-            q = self.getQValue(state, action)
+            q = self.get_q_value(state, action)
             q_list.append(q)
         if len(q_list) == 0:
             return 0
         return max(q_list)
 
-    def updateQ(self, state, action, reward, qmax):
+    # update the Q table values
+    def update_q_table(self, state, action, reward, qmax):
         discrete_state = self.get_discrete_state(state)
-        q = self.getQValue(state, action)
+        q = self.get_q_value(state, action)
         index = discrete_state + tuple([self.direction_map[action]])
-        self.q_value[index] = q + self.alpha * (reward + self.gamma * qmax - q)
+        self.q_table[index] = q + self.alpha * (reward + self.gamma * qmax - q)
 
+    # return the action that pacman must take
     def getAction(self, state):
         reward = state.getScore() - self.score
         if len(self.lastState) > 0:
             last_state = self.lastState[-1]
             last_action = self.lastAction[-1]
-            max_q = self.getMaxQ(state)
-            self.updateQ(last_state, last_action, reward, max_q)
+            max_q = self.get_max_q_value(state)
+            self.update_q_table(last_state, last_action, reward, max_q)
 
+        # epsilon-greedy
         if np.random.random() > self.epsilon:
             legal_actions = state.getLegalPacmanActions()
             actions = util.Counter()
             for action in legal_actions:
-                actions[action] = self.getQValue(state, action)
+                actions[action] = self.get_q_value(state, action)
             action = actions.argMax()
         else:
+            # prevent stop action when exploring
             legal_actions = state.getLegalPacmanActions()
             if Directions.STOP in legal_actions:
                 legal_actions.remove(Directions.STOP)
@@ -127,21 +143,17 @@ class QLearningAgent(Agent):
 
         return action
 
-    # Handle the end of episodes
-    #
-    # This is called by the game after a win or a loss.
+    # called after each episode
     def final(self, state):
-
         if state.isWin():
             self.games_won_last_100 += 1
 
-        # update Q-values
+        # update Q Table
         reward = state.getScore() - self.score
         last_state = self.lastState[-1]
         last_action = self.lastAction[-1]
-        self.updateQ(last_state, last_action, reward, 0)
+        self.update_q_table(last_state, last_action, reward, 0)
         self.print_stats(reward, state)
-
         self.last_100_scores.append(state.getScore())
 
         # reset attributes
@@ -149,9 +161,12 @@ class QLearningAgent(Agent):
         self.lastState = []
         self.lastAction = []
 
+        # update epsilon
         self.update_epsilon()
 
+        # increment games played
         self.increment_games_played()
+
         if self.get_games_played() % 100 == 0:
             if self.games_won_last_100 >= self.max_games_won_overall_100:
                 self.max_games_won_overall_100 = self.games_won_last_100
@@ -164,17 +179,20 @@ class QLearningAgent(Agent):
             self.set_alpha(0)
             self.set_epsilon(0)
 
+    # print stats per episode
     def print_stats(self, reward, state):
         print('Training episode: {0} Reward: {1} Number of actions: {2} Score: {3}'
               .format(self.games_played, reward, len(self.lastAction), state.getScore()))
 
+    # print state per 100 episodes
     def print_stats_100(self):
         print("\nCompleted %s runs of training" % self.get_games_played())
         print("Average Score: {}".format(np.average(self.last_100_scores)))
         print("Winning percentage: {}%".format(self.games_won_last_100))
         print("Max winning percentage so far: {}%\n".format(self.max_games_won_overall_100))
 
-    def getClosestFood(self, state):
+    # find the position of the closest food
+    def get_closest_food(self, state):
         food = state.getFood()
         walls = state.getWalls()
 
